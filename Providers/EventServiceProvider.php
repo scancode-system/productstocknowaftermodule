ffd3d0c463c53a0d515417cdd8607d230f7e1147
Your Name <you@example.com>
2020-01-14 22:04:03 -0300

    .

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -5,6 +5,8 @@ namespace Modules\ProductStockNowAfter\Providers;
 use Illuminate\Support\ServiceProvider;
 use Modules\Product\Events\AfterImportEvent;
 use Modules\ProductStockNowAfter\Listeners\AfterImportProductListener;
+use Modules\Product\Events\ProductLazyEagerLoadingEvent;
+use Modules\ProductStockNowAfter\Listeners\ProductLazyEagerLoadingListener;
 use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider 
@@ -18,6 +20,7 @@ class EventServiceProvider extends ServiceProvider
 	public function register() 
 	{
 		Event::listen(AfterImportEvent::class, AfterImportProductListener::class);
+		Event::listen(ProductLazyEagerLoadingEvent::class, ProductLazyEagerLoadingListener::class);
 	}
 
 }
